1069002002414be588e86352bebb8b1080d1c43b
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-03-22 13:47:29 +0530

    Update tf-module-rds/

--- vars.tf
+++ vars.tf
@@ -3,9 +3,12 @@ variable "engine" {}
 variable "database_name" {
   default = "dummy"
 }
+
 variable "engine_version" {}
 variable "subnet_ids" {}
 variable "tags" {}
 variable "backup_retention_period" {}
 variable "preferred_backup_window" {}
+variable "no_of_instances" {}
+variable "instance_class" {}
 
