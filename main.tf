e1bd874e1bc1f1cae87693e4d56b989515c1827f
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-03-22 17:06:20 +0530

    Update tf-module-rds/

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_rds_cluster" "main" {
   master_password         = data.aws_ssm_parameter.pass.value
   backup_retention_period = var.backup_retention_period
   preferred_backup_window = var.preferred_backup_window
-  db_subnet_group_name = aws_db_subnet_group.main.name
+  db_subnet_group_name    = aws_db_subnet_group.main.name
   skip_final_snapshot     = true
 
   tags = merge(
